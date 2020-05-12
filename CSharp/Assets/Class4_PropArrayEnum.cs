1121f8632b8472bb73512068c23192ab4c0bb08f
Lemonder791123 <63327445+Lemonder791123@users.noreply.github.com>
2020-05-12 16:05:26 +0800

    0512

---
+++ CSharp/Assets/Class4_PropArrayEnum.cs
@@ -0,0 +1,156 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+
+//定義列舉enum
+//定義在類別外可以讓所有類別使用
+//春天 0，夏天 1...
+public enum Season
+{
+    Spring, Summer, Fall, Winter 
+}
+
+
+public enum State
+{
+    Idle, Walk, Attack, Damage, Skill, Dead
+}
+
+
+
+// 自行更變編號
+public enum Day
+{
+    D1 = 7, D2 = 1, D3, D4, D5, D6, D7
+}
+
+
+
+public class Class4_PropArrayEnum : MonoBehaviour
+{
+
+    //使用列舉
+    //修飾詞 列舉名稱 自訂名稱
+    public Season _season;
+    public State _state;
+    public Day _day = Day.D3;
+
+    #region 練習陣列
+    public Ship ship;
+
+    //一般欄位
+    public string name1 = "KID";
+    public string name2 = "50ㄉㄉ";
+    public string name3 = "亮亮ㄉㄉ";
+
+
+
+    //陣列：類型[]
+    //屬性面板上 size 指的是數量
+    //屬性面板上的 Element 為陣列成員 編號從 0 開始
+    public string[] names;                                  //宣告陣列不指定數量
+
+    public float[] weights = new float[3];                  //宣告陣列並指定數量
+
+    public int[] scores = new int[] { 10, 20, 50, 50 };     // 宣告陣列並指定內容
+
+    public bool[] mission = { true, false, false };         // 宣告陣列並指定內容
+
+    #endregion
+
+
+    private void Awake()
+    {
+        //取得列舉
+        print("季節：" + _season);
+        print("狀態：" + (int)_state);     // (int) 將列舉轉型為整數
+        print("日期：" + (int)_day);
+       
+        //存放列舉
+        _state = State.Walk;
+     }
+
+
+
+
+
+
+
+
+    private void Start()
+    {
+        //設定屬性
+        ship.length = 99.9f;
+        //取得屬性
+        print("船的長度" + ship.length);
+
+
+        //設定唯獨屬性
+        //ship.width = 20.5f;   //錯誤
+        //取得唯獨屬性
+        print("船的寬度：" + ship.width);
+
+        ship.atk = 50.3f;
+        print("船的攻擊力：" + ship.atk);
+
+        ship.def = 10.1f;
+        print("船的防禦力：" + ship.def);
+
+
+        //指定陣列資料
+        names = new string[] { "A", "B", "C" };
+
+        //陣列[編號] - 注意編號從0開始
+        //陣列存放
+        scores[0] = 77;
+
+        //陣列取得
+        print("取得怪物 - 第三筆資料：" + names[2]);
+        //陣列常見錯誤：編號超出範圍
+        //indexOutOfRangeException
+
+        //陣列.Length 長度 (數量)
+        print("分數 - 陣列的長度 (數量)：" + scores.Length);
+
+
+
+        ChangeScore(scores);
+
+        TestValue(A);
+        TestRefe(B);
+
+     }
+
+    //陣列為參考型別
+    /// <summary>
+    /// 改變第二章分數為99分
+    /// </summary>
+   
+    private void ChangeScore(int[] theArray)
+    {
+        theArray[1] = 99;
+    }
+
+    //整數為實質型別
+    public int A = 99;
+    //陣列為參考型別
+    public int[] B = { 99 };
+
+
+    /// <summary>
+    /// 測試實質類別：傳值 - 傳資料進來，不會修改原本資料
+    /// </summary>
+    private void TestValue(int test)
+    {
+        test = 0;
+    }
+
+
+    /// <summary>
+    /// 測試參考類別：傳址 - 傳地址進來，不會修改原本資料
+    /// </summary>
+    private void TestRefe(int[] test)
+    {
+        test [0] = 0;
+    }
+}
---
+++ CSharp/Assets/Ship.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+
+public class Ship : MonoBehaviour
+{
+    //公開欄位：給任何人存取
+    //從其他類別取得或存放資料
+    public float speed = 20;
+
+
+    //私人欄位：僅限於此類別存取
+    //從其他類別無法取得或存放資料
+    private float weight = 20;
+
+
+
+    //問題：
+    //假設需要取得但是不能修改－屬性（權限）
+    //屬性語法
+    //修飾詞 類型 名稱 { 存取權限 }
+    //get可以取得
+    //set可以存放
+    //讀寫權限屬性
+    //屬性不會顯示在面板上，
+    public float length { get; set; }
+
+
+    //舉例 - 企劃：寬度固定 30 不變
+    //使用封裝欄位 - 須具備條件:
+    //1.私人欄位
+    //2.唯獨屬性(return私人欄位)
+    private float _width = 30;
+    //唯讀屬性:不能設定值
+    public float width { get { return _width; } }
+
+    //企劃：寬度固定　10.5 不變
+    private float _height=10.5f;
+
+    // =>(黏巴達 Lambda)
+    // C#6 版以上| Unity 2017 C#7 版
+    // C# 改版 8 版
+    // get => _height; 等同於 get { return _height; }
+    public float height { get => _height; }
+    
+
+    public float lv = 3;
+    //企劃：攻擊力等於　攻擊力＊等級
+    //封裝欄位：
+    //１.私人欄位
+    //2.透過屬性存取(return私人欄位){私人欄位 = value}
+    //value 設定屬性時給予值
+    private float _atk;
+    public float atk { get { return _atk; } set { _atk = value * lv; } }
+
+    private float _def;
+    public float def { get => _def; set => _def = value * lv; }
+    
+}
